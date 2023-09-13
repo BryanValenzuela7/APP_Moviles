02a56f50fd79a2efaa0a4bc42ce265ceee8a124b
BryanValenzuela7 <bryan3d2y@gmail.com>
2023-09-12 17:34:43 -0700

    Agregado boton, status bar, scroll view y estilos

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet,Modal, SafeAreaView, Text, Button, TextInput, View, Pressable } from "react-native"
+import { StyleSheet,Modal, SafeAreaView, Text, Button, TextInput, View, Pressable, StatusBar, ScrollView } from "react-native"
 import CustomButton from "./Button"
 import DatePicker from "react-native-date-picker"
 import React, { useState } from 'react';
@@ -7,10 +7,18 @@ const Formulario =({modalVisible, newDateHandler}) =>{
 
     return(
         <Modal animationType='slide' visible={modalVisible}>
+            <StatusBar backgroundColor={'#333'}/>
+            <ScrollView>
         <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Nueva {''}
             <Text style={styles.titleBold}>Cita</Text>
         </Text>
+        <CustomButton title ={'CANCELAR'}
+        onPress={()=>{
+            newDateHandler()
+        }}
+        customColor={'#edede9'}
+        />
         <View style={styles.group}>
             <Text style={styles.label}>Nombre Paciente</Text>
             <TextInput 
@@ -18,25 +26,32 @@ const Formulario =({modalVisible, newDateHandler}) =>{
             placeholder="Nombre paciente" 
             placeholderTextColor={''}/>
 
-            <Text style={styles.label}>Nombre Paciente</Text>
+            <Text style={styles.label}>Nombre propietario</Text>
             <TextInput 
             style={styles.input}
-            placeholder="Nombre paciente" 
+            placeholder="Nombre propietario" 
             placeholderTextColor={''}/>
 
-            <Text style={styles.label}>Nombre ghj</Text>
+            <Text style={styles.label}>Email propietario</Text>
             <TextInput 
             style={styles.input}
-            placeholder="Nombre paciente" 
-            placeholderTextColor={''}/>
+            placeholder="Email propietario" 
+            placeholderTextColor={''}
+            keyboardType='email-address'
+            />
+            
         </View>
         <View style={styles.group}>
             <Text style={styles.label}>Fecha</Text>
             <DatePicker 
             date={date}
-            
-            />
+        />
         </View>
+        <TextInput style={[styles.input, styles.inputSymptoms]}
+        placeholder="Sintomas"
+        multiline ={true}
+        placeholderTextColor={"#666"}
+        />
         <CustomButton title ={'Registrar'}
         onPress={()=>{
             newDateHandler()
@@ -44,6 +59,7 @@ const Formulario =({modalVisible, newDateHandler}) =>{
         customColor={'#edede9'}
         />
         </SafeAreaView>
+        </ScrollView>
       </Modal>
     )
 }
@@ -78,6 +94,10 @@ const styles = StyleSheet.create({
     titleBold:{
         fontWeight: '900'
     },
+    inputSymptoms:{
+        height: 100,
+        marginBottom: 20,
+    }
 
 })
 export default Formulario
